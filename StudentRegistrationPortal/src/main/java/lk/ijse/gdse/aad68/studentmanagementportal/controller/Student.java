ee073b8495ae28ba45c7c3d350dbdca0aba0e862
Induwara <induwara2k@gmail.com>
2024-07-28 13:00:22 +0530

    refactor: implement layered

--- StudentRegistrationPortal/src/main/java/lk/ijse/gdse/aad68/studentmanagementportal/controller/Student.java
+++ StudentRegistrationPortal/src/main/java/lk/ijse/gdse/aad68/studentmanagementportal/controller/Student.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.gdse.aad68.studentmanagementportal.dao.StudentDAOImpl;
 import lk.ijse.gdse.aad68.studentmanagementportal.dto.StudentDTO;
 import lk.ijse.gdse.aad68.studentmanagementportal.util.Util;
 import org.slf4j.Logger;
@@ -50,8 +51,9 @@ public class Student extends HttpServlet {
             var ctx = new InitialContext();
             DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/StudentPortal");
             this.connection = pool.getConnection();
-            logger.info("Connection initialized",this.connection);
+            logger.debug("Connection initialized",this.connection);
         }catch ( SQLException | NamingException e){
+            logger.error("DB connection not init" );
             e.printStackTrace();
         }
     }
@@ -64,24 +66,13 @@ public class Student extends HttpServlet {
 
         try (var writer = resp.getWriter()){
             Jsonb jsonb = JsonbBuilder.create();
+            var studentDAOIMPL = new StudentDAOImpl();
             StudentDTO student = jsonb.fromJson(req.getReader(), StudentDTO.class);
             student.setId(Util.idGenerate());
             //Save data in the DB
-            var ps = connection.prepareStatement(SAVE_STUDENT);
-            ps.setString(1, student.getId());
-            ps.setString(2, student.getName());
-            ps.setString(3, student.getEmail());
-            ps.setString(4, student.getCity());
-            ps.setString(5, student.getLevel());
-            if(ps.executeUpdate() != 0){
-                resp.setStatus(HttpServletResponse.SC_CREATED);
-                writer.write("Save Student Successfully");
-
-            }else {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                writer.write("Failed to Save Student");
-            }
-        }catch (SQLException e){
+            writer.write(studentDAOIMPL.saveStudent(student,connection));
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+        }catch (Exception e){
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             e.printStackTrace();
         }
